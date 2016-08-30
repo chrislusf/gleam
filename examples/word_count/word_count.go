4d6dda3d9573df76b2c137897e6a1bf96f6780b8
Chris Lu <chris.lu@gmail.com>
2016-08-30 02:06:07 -0700

    fancier word count

--- examples/word_count/word_count.go
+++ examples/word_count/word_count.go
@@ -6,14 +6,28 @@ import (
 )
 
 func main() {
-	f := flow.New()
+	shellFlow := flow.New()
 
-	ch := f.TextFile("/etc/passwd").Script("sh").Map("grep -v window").Map("awk {print}").Output()
-	// ch := f.TextFile("/etc/passwd").Script("sh").Map("sort").Output()
+	shellOutChan := shellFlow.TextFile("/etc/passwd").Script("sh").Map("grep -v window").Map("awk {print}").Output()
+	// shellOutChan := shellFlow.TextFile("/etc/passwd").Script("sh").Map("sort").Output()
 
-	go flow.RunFlowContextSync(f)
+	go flow.RunFlowContextSync(shellFlow)
 
-	for bytes := range ch {
+	for bytes := range shellOutChan {
+		println("shell:", string(bytes))
+	}
+
+	luaFlow := flow.New()
+	luaOutChan := luaFlow.TextFile("/etc/passwd").Script("lua").Map(`
+		function(line)
+			for w in line:gmatch("%w+") do print(w) end
+		end
+	`).Script("sh").Map("sort").Map("uniq -c").Map("sort").Output()
+
+	go flow.RunFlowContextSync(luaFlow)
+
+	for bytes := range luaOutChan {
 		println(string(bytes))
 	}
+
 }
