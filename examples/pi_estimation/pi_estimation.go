b20f306b8e31c23c9c2e92019724835210317b6b
Chris Lu <chris.lu@gmail.com>
2016-09-30 22:59:27 -0700

    add perf compare

--- examples/pi_estimation/pi_estimation.go
+++ examples/pi_estimation/pi_estimation.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/chrislusf/gleam"
 	"github.com/chrislusf/gleam/util"
+	"github.com/chrislusf/glow/flow"
 )
 
 func main() {
 
+	times := 1024 * 1024 * 10
+
+	testPureGo(times)
+	testLuajit(times)
+	testFlow(times)
+
+	testDistributedGleam(times)
+	testLocalGleam(times)
+}
+
+func testDistributedGleam(times int) {
 	var count int64
-	times := 1024 * 1024 * 1
 
 	startTime := time.Now()
 	gleam.NewDistributed().Script("lua", `
@@ -28,9 +40,13 @@ func main() {
     `).Reduce("count").SaveOneRowTo(&count)
 
 	fmt.Printf("pi = %f\n", 4.0*float64(count)/float64(times))
-	fmt.Printf("time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Printf("gleam distributed time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Println()
+}
 
-	startTime = time.Now()
+func testLocalGleam(times int) {
+	var count int64
+	startTime := time.Now()
 	gleam.New().Script("lua", `
       function count(x, y)
         return x + y
@@ -45,6 +61,71 @@ func main() {
     `).Reduce("count").SaveOneRowTo(&count)
 
 	fmt.Printf("pi = %f\n", 4.0*float64(count)/float64(times))
-	fmt.Printf("time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Printf("gleam local time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Println()
+}
 
+func testFlow(times int) {
+	startTime := time.Now()
+	ch := make(chan int)
+	go func() {
+		for i := 0; i < times; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+	flow.New().Channel(ch).Map(func(t int) int {
+		x, y := rand.Float32(), rand.Float32()
+		if x*x+y*y < 1 {
+			return 1
+		}
+		return 0
+	}).LocalReduce(func(x, y int) int {
+		return x + y
+	}).Map(func(count int) {
+		println("=>", count)
+		fmt.Printf("pi = %f\n", 4.0*float64(count)/float64(times))
+	}).Run()
+
+	fmt.Printf("flow time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Println()
+}
+
+func testPureGo(times int) {
+	startTime := time.Now()
+
+	var count int64
+	for i := 0; i < times; i++ {
+		x, y := rand.Float64(), rand.Float64()
+		if x*x+y*y < 1 {
+			count++
+		}
+	}
+	println("=>", count)
+	fmt.Printf("pi = %f\n", 4.0*float64(count)/float64(times))
+	fmt.Printf("pure go time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Println()
+}
+
+func testLuajit(times int) {
+	startTime := time.Now()
+	var count int64
+
+	startTime = time.Now()
+	gleam.New().Source(util.Range(1, 1, 1)).Map(fmt.Sprintf(`
+      function(n)
+	    local count = 0
+	    for i=1,%d,1 do
+          local x, y = math.random(), math.random()
+          if x*x+y*y < 1 then
+            count = count + 1
+          end
+		end
+		return count
+      end
+    `, times)).SaveOneRowTo(&count)
+
+	fmt.Printf("pi = %f\n", 4.0*float64(count)/float64(times))
+	fmt.Printf("luajit local time diff: %s\n", time.Now().Sub(startTime))
+	fmt.Println()
 }
