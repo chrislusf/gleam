7bb856e023f9e01f94e9cc46637e0f539abe074c
Chris Lu <chris.lu@gmail.com>
2016-10-14 22:43:45 -0700

    robust csv parser

--- source/csv/csv.go
+++ source/csv/csv.go
@@ -7,43 +7,90 @@ import (
 	"github.com/chrislusf/gleam/flow"
 )
 
-func Read(f *flow.FlowContext, fileNames []string, parallel int, separator string,
+func Read(f *flow.FlowContext, fileNames []string, parallel int,
 	hasHeader bool, fieldNames ...string) *flow.Dataset {
-	if separator == "\t" {
-		separator = "\\t"
-	}
 
-	readFile := fmt.Sprintf(`
+	declareCode := `
+-- http://lua-users.org/wiki/CsvUtils
+-- Used to escape "'s by toCSV
+function escapeCSV (s)
+  if string.find(s, '[,"]') then
+    s = '"' .. string.gsub(s, '"', '""') .. '"'
+  end
+  return s
+end
+
+-- Convert from CSV string to table (converts a single line of a CSV file)
+function fromCSV(s)
+  s = s .. ','        -- ending comma
+  local t = {}        -- table to collect fields
+  local fieldstart = 1
+  repeat
+    -- next field is quoted? (start with '"'?)
+    if string.find(s, '^"', fieldstart) then
+      local a, c
+      local i  = fieldstart
+      repeat
+        -- find closing quote
+        a, i, c = string.find(s, '"("?)', i+1)
+      until c ~= '"'    -- quote not followed by quote?
+      if not i then error('unmatched "') end
+      local f = string.sub(s, fieldstart+1, i-1)
+      table.insert(t, (string.gsub(f, '""', '"')))
+      fieldstart = string.find(s, ',', i) + 1
+    elseif string.find(s, "^'", fieldstart) then
+      local a, c
+      local i  = fieldstart
+      repeat
+        -- find closing quote
+        a, i, c = string.find(s, "'('?)", i+1)
+      until c ~= "'"    -- quote not followed by quote?
+      if not i then error("unmatched '") end
+      local f = string.sub(s, fieldstart+1, i-1)
+      table.insert(t, (string.gsub(f, "''", "'")))
+      fieldstart = string.find(s, ',', i) + 1
+    else                -- unquoted; find next comma
+      local nexti = string.find(s, ',', fieldstart)
+      table.insert(t, string.sub(s, fieldstart, nexti-1))
+      fieldstart = nexti + 1
+    end
+  until fieldstart > string.len(s)
+  return t
+end
+
+-- Convert from table to CSV string
+function toCSV (tt)
+  local s = ""
+  for _,p in ipairs(tt) do  
+    s = s .. "," .. escapeCSV(p)
+  end
+  return string.sub(s, 2)      -- remove first comma
+end
+`
+
+	readFile := `
         for line in csvFile:lines('*l') do
-          local l = {}
-          for t in split(line, '%s') do
-            table.insert(l,t)
-          end
+          local l = fromCSV(line)
           writeRow(unpack(l))
         end
-    `, separator)
+    `
 
 	if hasHeader {
 		if len(fieldNames) == 0 {
-			readFile = fmt.Sprintf(`
+			readFile = `
                 local header = csvFile:read()
 
                 for line in csvFile:lines() do
-                  local l = {}
-                  for t in split(line, '%s') do
-                    table.insert(l,t)
-                  end
+                  local l = fromCSV(line)
                   writeRow(unpack(l))
                 end
-            `, separator)
+            `
 		} else {
 			readFile = fmt.Sprintf(`
-                local header = csvFile:read()
+                local header = fromCSV(csvFile:read())
                 local header2position = {}
-                local x = 1
-                for h in split(header, '%s') do
+                for x, h in ipairs(header) do
                   header2position[h] = x
-                  x = x + 1
                 end
                 
                 local fields = {%s}
@@ -53,17 +100,14 @@ func Read(f *flow.FlowContext, fileNames []string, parallel int, separator strin
                 end
     
                 for line in csvFile:lines() do
-                  local l = {}
-                  for t in split(line, '%s') do
-                    table.insert(l,t)
-                  end
+                  local l = fromCSV(line)
                   local row = {}
                   for _, x in ipairs(indexes) do
                     table.insert(row, l[x])
                   end
                   writeRow(unpack(row))
                 end
-            `, separator, toFieldNameList(fieldNames), separator)
+            `, toFieldNameList(fieldNames))
 		}
 	}
 
@@ -75,7 +119,7 @@ func Read(f *flow.FlowContext, fileNames []string, parallel int, separator strin
         end
     `, readFile)
 
-	return f.Strings(fileNames).Partition(parallel).ForEach(actualCode)
+	return f.Strings(fileNames).Partition(parallel).Script("lua", declareCode).ForEach(actualCode)
 }
 
 func toFieldNameList(fieldNames []string) string {
--- source/csv/csv_test.go
+++ source/csv/csv_test.go
@@ -10,21 +10,21 @@ import (
 func TestReadWithHeader(t *testing.T) {
 	f := gleam.New()
 
-	data := Read(f, []string{"sample1.csv"}, 1, ",", true)
+	data := Read(f, []string{"sample1.csv"}, 1, true)
 	data.Select(2, 3, 1).Fprintf(os.Stderr, "%s,%s,%s\n").Run()
 }
 
 func TestReadWithoutHeader(t *testing.T) {
 	f := gleam.New()
 
-	data := Read(f, []string{"sample0.csv"}, 1, ",", false)
+	data := Read(f, []string{"sample0.csv"}, 1, false)
 	data.Select(2, 3, 1, 1).Fprintf(os.Stderr, "%s,%s,%s,%s\n").Run()
 }
 
 func TestReadHeaderByFieldNames(t *testing.T) {
 	f := gleam.New()
 
-	data := Read(f, []string{"sample2.csv"}, 1, ",", true,
+	data := Read(f, []string{"sample2.csv"}, 1, true,
 		"statecode", "line", "construction", "policyID")
 	data.Fprintf(os.Stderr, "%s,%s,%s,%s\n").Run()
 }
