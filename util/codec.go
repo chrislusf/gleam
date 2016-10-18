a6ed57e58ff02509915c7c11fc3471a8beeaad54
Chris Lu <chris.lu@gmail.com>
2016-10-18 00:32:35 -0700

    remove json printer

--- util/codec.go
+++ util/codec.go
@@ -8,32 +8,6 @@ import (
 	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
-type Encoder interface {
-	Encode(v interface{}) error
-}
-
-type Decoder interface {
-	Decode(v interface{}) error
-}
-
-func DecodeToObject(msgpackBytes []byte) (object interface{}, err error) {
-
-	decoder := NewMsgpackDecoderBytes(msgpackBytes)
-
-	err = decoder.Decode(&object)
-
-	return
-}
-
-func PrintAsJSON(object interface{}, writer io.Writer, isPrettyPrint bool) error {
-	encoder := NewJSONEncoder(writer, isPrettyPrint)
-	if err := encoder.Encode(object); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // encode and write
 func WriteRow(outChan io.Writer, anyObject ...interface{}) error {
 	encoded, err := EncodeRow(anyObject...)
