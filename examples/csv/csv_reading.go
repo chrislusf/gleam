ca86efd4a371bbd81d07ef06b6b7ed72b7f58704
Chris Lu <chris.lu@gmail.com>
2016-10-18 23:03:04 -0700

    csv file join

--- examples/csv/csv_reading.go
+++ examples/csv/csv_reading.go
@@ -9,13 +9,23 @@ import (
 
 func main() {
 
-	f := gleam.New(gleam.Distributed)
-	f.Input(&csv.CsvInput{
+	f := gleam.New(gleam.Local)
+	a := f.Input(&csv.CsvInput{
 		FileNames: []string{
 			"/Users/chris/dev/gopath/src/github.com/chrislusf/gleam/examples/csv/sample0.csv",
+			"/Users/chris/dev/gopath/src/github.com/chrislusf/gleam/examples/csv/sample1.csv",
+			"/Users/chris/dev/gopath/src/github.com/chrislusf/gleam/examples/csv/sample2.csv",
+		},
+		HasHeader: true,
+	}, 1).Select(1, 2, 3)
+
+	b := f.Input(&csv.CsvInput{
+		FileNames: []string{
 			"/Users/chris/dev/gopath/src/github.com/chrislusf/gleam/examples/csv/sample0.csv",
 		},
 		HasHeader: true,
-	}, 1).Select(1, 2, 3).Fprintf(os.Stdout, "%s : %s %s\n").Run()
+	}, 1).Select(1, 4, 5)
+
+	a.Join(b).Fprintf(os.Stdout, "%s : %s %s, %s %s\n").Run()
 
 }
