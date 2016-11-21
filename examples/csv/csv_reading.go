a27bb8ca37fe3524db629891ac8d8cfce6580e94
Chris Lu <chris.lu@gmail.com>
2016-11-20 17:17:38 -0800

    minor adjust

--- examples/csv/csv_reading.go
+++ examples/csv/csv_reading.go
@@ -11,10 +11,15 @@ import (
 func main() {
 
 	f := New()
+
 	a := f.ReadFile(csv.New("a?.csv").SetHasHeader(true)).Select(Field(1, 2, 3))
 
 	b := f.ReadFile(csv.New("b*.csv")).Select(Field(1, 4, 5))
 
-	a.RightOuterJoin(b).Fprintf(os.Stdout, "%s : %s %s, %s %s\n").Run(distributed.Option())
+	join := a.RightOuterJoin(b).Fprintf(os.Stdout, "%s : %s %s, %s %s\n")
+
+	// join.Run(distributed.Planner())
+
+	join.Run(distributed.Option())
 
 }
