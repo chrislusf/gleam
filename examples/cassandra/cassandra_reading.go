9d7d427d95c671258d2efb32185916c5c7b171d1
Chris Lu <chris.lu@gmail.com>
2016-11-20 17:17:15 -0800

    minor adjustment

--- examples/cassandra/cassandra_reading.go
+++ examples/cassandra/cassandra_reading.go
@@ -15,13 +15,15 @@ func main() {
 			Set("keyspace", "system").Set("hosts", "localhost")
 	*/
 
-	f := flow.New()
-	f.Query("connection1", &cassandra.Query{
+	f := flow.New().Query("connection1", &cassandra.Query{
 		Select:   "key, host_id, release_version, rpc_address, schema_version, tokens",
 		Keyspace: "system",
 		Table:    "local",
 		Parallel: 8,
-	}).Fprintf(os.Stdout, "key:%s\nhost_id:%v\nrelease_version:%s, %s, \nschema_version:%+v,\n%s \n").
-		Run(distributed.Option())
+	}).Fprintf(os.Stdout, "key:%s\nhost_id:%v\nrelease_version:%s, %s, \nschema_version:%+v,\n%s \n")
+
+	// f.Run(distributed.Planner())
+
+	f.Run(distributed.Option())
 
 }
