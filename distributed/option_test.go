335b854cd034c0452944b685ad2bcc38b85a3fd6
Chris Lu <chris.lu@gmail.com>
2016-12-24 10:55:11 -0800

    fix the test

--- distributed/option_test.go
+++ distributed/option_test.go
@@ -5,45 +5,19 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-	"time"
 
-	"github.com/chrislusf/gleam/distributed/agent"
-	"github.com/chrislusf/gleam/distributed/master"
 	"github.com/chrislusf/gleam/flow"
-	"github.com/golang/protobuf/proto"
 )
 
-func xxTestInstructionSet(t *testing.T) {
+func TestInstructionSet(t *testing.T) {
 
-	masterPort := ":34563"
-	masterAddress := "localhost" + masterPort
-
-	go master.RunMaster(masterPort)
-
-	time.Sleep(time.Second)
-
-	go agent.NewAgentServer(&agent.AgentServerOption{
-		Dir:          proto.String("."),
-		Host:         proto.String("localhost"),
-		Port:         proto.Int(6666),
-		Master:       proto.String(masterAddress),
-		DataCenter:   proto.String("defaultDataCenter"),
-		Rack:         proto.String("defaultRack"),
-		MaxExecutor:  proto.Int(8),
-		CPULevel:     proto.Int(1),
-		MemoryMB:     proto.Int64(1024),
-		CleanRestart: proto.Bool(true),
-	}).Run()
-
-	time.Sleep(time.Second)
-
-	fileNames, err := filepath.Glob("../../flow/*.go")
+	fileNames, err := filepath.Glob("../flow/*.go")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	f := flow.New()
-	f.Strings(fileNames).Partition(3).PipeAsArgs("ls -l $1").FlatMap(`
+	f.Strings(fileNames).Partition(3).PipeAsArgs("cat $1").FlatMap(`
       function(line)
         return line:gmatch("%w+")
       end
@@ -61,7 +35,7 @@ func xxTestInstructionSet(t *testing.T) {
       end
     `).Pipe("sort -n -k 2").Fprintf(os.Stdout, "%s\n")
 
-	f.Run(Option().SetMaster(masterAddress))
+	f.Run()
 
 	os.Exit(0)
 
