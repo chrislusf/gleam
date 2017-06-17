529a541b6b505578e0d363dc0d75362dec10a48a
Chris Lu <chris.lu@gmail.com>
2017-06-17 08:43:44 -0700

    optional distributed mode

--- examples/cassandra_reader/cassandra_reader.go
+++ examples/cassandra_reader/cassandra_reader.go
@@ -3,22 +3,24 @@ package main
 import (
 	"flag"
 
+	"github.com/chrislusf/gleam/distributed"
 	"github.com/chrislusf/gleam/flow"
 	"github.com/chrislusf/gleam/gio"
 	"github.com/chrislusf/gleam/plugins/cassandra"
 )
 
 var (
-	hosts        = flag.String("hosts", "127.0.0.1", "a list of comma separated host names")
-	keyspace     = flag.String("keyspace", "system_schema", "the keyspace containing the table")
-	table        = flag.String("from", "tables", "the table name")
-	selectClause = flag.String("select", "keyspace_name, table_name", "a list of field names")
-	where        = flag.String("where", "", "optional where clause")
-	limit        = flag.Int("limit", 10, "the number of rows to return. 0 means unlimited.")
-	format       = flag.String("format", "table: %s keyspace:%s", "formatted output result")
-	timeout      = flag.Int("timeout", 30, "the number of seconds for timeout connections")
-	concurrency  = flag.Int("concurrent", 2, "the number of concurrent read processes")
-	shardCount   = flag.Int("shardCount", 4, "the number of shards to partition the data into")
+	hosts         = flag.String("hosts", "127.0.0.1", "a list of comma separated host names")
+	keyspace      = flag.String("keyspace", "system_schema", "the keyspace containing the table")
+	table         = flag.String("from", "tables", "the table name")
+	selectClause  = flag.String("select", "keyspace_name, table_name", "a list of field names")
+	where         = flag.String("where", "", "optional where clause")
+	limit         = flag.Int("limit", 10, "the number of rows to return. 0 means unlimited.")
+	format        = flag.String("format", "table: %s keyspace:%s", "formatted output result")
+	timeout       = flag.Int("timeout", 30, "the number of seconds for timeout connections")
+	concurrency   = flag.Int("concurrent", 2, "the number of concurrent read processes")
+	shardCount    = flag.Int("shardCount", 4, "the number of shards to partition the data into")
+	isDistributed = flag.Bool("distributed", false, "run in distributed mode")
 )
 
 func main() {
@@ -34,6 +36,10 @@ func main() {
 
 	f := flow.New().Read(c).Sort(flow.Field(2)).Select(flow.Field(2, 1)).Printlnf(*format)
 
-	f.Run()
+	if *isDistributed {
+		f.Run(distributed.Option())
+	} else {
+		f.Run()
+	}
 
 }
