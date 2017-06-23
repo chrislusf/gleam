336412a1b8aa95edf1771a758a8754007852bff6
Chris Lu <chris.lu@gmail.com>
2017-06-22 21:05:31 -0700

    adjust CLI options

--- examples/cassandra_reader/cassandra_reader.go
+++ examples/cassandra_reader/cassandra_reader.go
@@ -10,17 +10,17 @@ import (
 )
 
 var (
-	hosts         = flag.String("hosts", "127.0.0.1", "a list of comma separated host names")
-	keyspace      = flag.String("keyspace", "system_schema", "the keyspace containing the table")
-	table         = flag.String("from", "tables", "the table name")
-	selectClause  = flag.String("select", "keyspace_name, table_name", "a list of field names")
-	where         = flag.String("where", "", "optional where clause")
-	limit         = flag.Int("limit", 10, "the number of rows to return. 0 means unlimited.")
-	format        = flag.String("format", "table: %s keyspace:%s", "formatted output result")
-	timeout       = flag.Int("timeout", 30, "the number of seconds for timeout connections")
-	concurrency   = flag.Int("concurrent", 2, "the number of concurrent read processes")
-	shardCount    = flag.Int("shardCount", 4, "the number of shards to partition the data into")
-	isDistributed = flag.Bool("distributed", false, "run in distributed mode")
+	hosts            = flag.String("hosts", "127.0.0.1", "a list of comma separated host names")
+	keyspace         = flag.String("keyspace", "system_schema", "the keyspace containing the table")
+	table            = flag.String("from", "tables", "the table name")
+	selectClause     = flag.String("select", "keyspace_name, table_name", "a list of field names")
+	where            = flag.String("where", "", "optional where clause")
+	LimitInEachShard = flag.Int("limitInEachShard", 0, "the number of rows to return for each shard. 0 means unlimited.")
+	format           = flag.String("format", "table: %s keyspace:%s", "formatted output result")
+	timeout          = flag.Int("timeout", 30, "the number of seconds for timeout connections")
+	concurrency      = flag.Int("concurrent", 2, "the number of concurrent read processes")
+	shardCount       = flag.Int("shardCount", 4, "the number of shards to partition the data into")
+	isDistributed    = flag.Bool("distributed", false, "run in distributed mode")
 )
 
 func main() {
@@ -31,7 +31,7 @@ func main() {
 	c := cassandra.Hosts(*hosts).Keyspace(*keyspace).Select(*selectClause).From(*table).Where(*where)
 	c.ShardCount = *shardCount
 	c.Concurrency = *concurrency
-	c.LimitInEachShard = *limit
+	c.LimitInEachShard = *LimitInEachShard
 	c.TimeoutSeconds = *timeout
 
 	f := flow.New().Read(c).Sort(flow.Field(2)).Select(flow.Field(2, 1)).Printlnf(*format)
