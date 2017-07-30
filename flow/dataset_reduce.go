5876afe3700556970585fabf5cf97ee59563cb32
Chris Lu <chris.lu@gmail.com>
2017-07-30 02:29:45 -0700

    tmp commit, "flow" package now compiles

--- flow/dataset_reduce.go
+++ flow/dataset_reduce.go
@@ -10,65 +10,24 @@ import (
 	"github.com/chrislusf/gleam/script"
 )
 
-func (d *Dataset) Reduce(code string) (ret *Dataset) {
-	ret = d.LocalReduce(code)
-	if len(d.Shards) > 1 {
-		sortOption := Field(1)
-		ret = ret.MergeSortedTo(1, sortOption).LocalReduce(code)
-		ret.IsLocalSorted = sortOption.orderByList
-	}
-	return ret
-}
-
-func (d *Dataset) LocalReduce(code string) *Dataset {
-	ret, step := add1ShardTo1Step(d)
-	ret.IsLocalSorted = d.IsLocalSorted
-	ret.IsPartitionedBy = d.IsPartitionedBy
-	step.Name = "LocalReduce"
-	step.Script = d.Flow.createScript()
-	step.Script.Reduce(code)
-	return ret
-}
-
-func (d *Dataset) ReduceBy(code string, sortOptions ...*SortOption) (ret *Dataset) {
-	sortOption := concat(sortOptions)
-
-	ret = d.LocalSort(sortOption).LocalReduceBy(code, sortOption)
-	if len(d.Shards) > 1 {
-		ret = ret.MergeSortedTo(1, sortOption).LocalReduceBy(code, sortOption)
-	}
-	return ret
-}
-
 // ReducerBy runs the reducer registered to the reducerId.
 // This is used to execute pure Go code.
-func (d *Dataset) ReducerBy(reducerId gio.ReducerId, sortOptions ...*SortOption) (ret *Dataset) {
+func (d *Dataset) ReduceBy(name string, reducerId gio.ReducerId, sortOptions ...*SortOption) (ret *Dataset) {
 	sortOption := concat(sortOptions)
 
-	ret = d.LocalSort(sortOption).LocalReducerBy(reducerId, sortOption)
+	ret = d.LocalSort(sortOption).LocalReduceBy(name+".local2", reducerId, sortOption)
 	if len(d.Shards) > 1 {
-		ret = ret.MergeSortedTo(1, sortOption).LocalReducerBy(reducerId, sortOption)
+		ret = ret.MergeSortedTo(1, sortOption).LocalReduceBy(name+".local2", reducerId, sortOption)
 	}
 	return ret
 }
 
-func (d *Dataset) LocalReduceBy(code string, sortOptions ...*SortOption) *Dataset {
-	sortOption := concat(sortOptions)
-
-	ret, step := add1ShardTo1Step(d)
-	// TODO calculate IsLocalSorted IsPartitionedBy based on indexes
-	step.Name = "LocalReduceBy"
-	step.Script = d.Flow.createScript()
-	step.Script.ReduceBy(code, sortOption.Indexes())
-	return ret
-}
-
-func (d *Dataset) LocalReducerBy(reducerId gio.ReducerId, sortOptions ...*SortOption) *Dataset {
+func (d *Dataset) LocalReduceBy(name string, reducerId gio.ReducerId, sortOptions ...*SortOption) *Dataset {
 
 	sortOption := concat(sortOptions)
 
 	ret, step := add1ShardTo1Step(d)
-	step.Name = "LocalReducerBy"
+	step.Name = name
 	step.IsPipe = false
 	step.IsGoCode = true
 
