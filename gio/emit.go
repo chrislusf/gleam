2db63017a412293d132919d606070c6bfb6e2317
Chris Lu <chris.lu@gmail.com>
2017-08-06 18:58:04 -0700

    add TsEmitKV()

--- gio/emit.go
+++ gio/emit.go
@@ -17,3 +17,8 @@ func TsEmit(ts int64, anyObject ...interface{}) error {
 	stat.Stats[0].OutputCounter++
 	return util.NewRow(ts, anyObject...).WriteTo(os.Stdout)
 }
+
+func TsEmitKV(ts int64, keys, values []interface{}) error {
+	stat.Stats[0].OutputCounter++
+	return util.NewRow(ts).AppendKey(keys...).AppendValue(values...).WriteTo(os.Stdout)
+}
