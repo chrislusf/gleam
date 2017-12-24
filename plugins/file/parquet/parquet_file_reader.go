b8859cb17c0d35a6e7b6544c151303d87a27491f
xitongsys <xitongsys@gmail.com>
2017-12-24 10:23:26 +0800

    update for parquet-go1.1.0

--- plugins/file/parquet/parquet_file_reader.go
+++ plugins/file/parquet/parquet_file_reader.go
@@ -68,9 +68,13 @@ func (self *ParquetFileReader) Read() (row *util.Row, err error) {
 	}
 	objects := make([]interface{}, 0)
 	for _, fieldName := range self.pqReader.SchemaHandler.ValueColumns {
+		schemaIndex := self.pqReader.SchemaHandler.MapIndex[fieldName]
 		values := make([]interface{}, 1)
 		self.pqReader.ReadColumnByPath(fieldName, &values)
-		objects = append(objects, ParquetTypeToGoType(values[0]))
+		objects = append(objects, ParquetTypeToGoType(values[0],
+			self.pqReader.SchemaHandler.SchemaElements[schemaIndex].Type,
+			self.pqReader.SchemaHandler.SchemaElements[schemaIndex].ConvertedType,
+		))
 	}
 	self.Cursor++
 	return util.NewRow(util.Now(), objects...), nil
